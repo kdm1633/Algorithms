4c4f3a1b98a3a3c81ee33b3fb4c7e639816d8b34
kdm1633 <kdm1633@outlook.com>
2016-03-22 19:55:19 +0900

    Update Substitute.java

--- Substitute.java
+++ Substitute.java
@@ -1,24 +1,24 @@
 public class Substitute
 {	
-    public static int getValue(String key, String code) {
-        String dval = "";
-        int result	;
-        
-        key = key.substring(9) + key.substring(0,9);
-
-		// Eliminate cases that don't exist in variable key.
-        for(int i=0; i < code.length(); i++) {
-            if(key.indexOf(code.charAt(i)) != -1)
-                dval += code.charAt(i);
-        }
-        
-		result = key.indexOf(dval.charAt(0));
-        
-        for(int i=1; i < dval.length(); i++)
-			result = (result * 10) + key.indexOf(dval.charAt(i));
-        
-        return result;
-    }
+	public static int getValue(String key, String code) {
+	String dval = "";
+	int result	;
+	
+	key = key.substring(9) + key.substring(0,9);
+	
+	// Eliminate cases that don't exist in variable key.
+	for(int i=0; i < code.length(); i++) {
+	    if(key.indexOf(code.charAt(i)) != -1)
+	        dval += code.charAt(i);
+	}
+	
+	result = key.indexOf(dval.charAt(0));
+	
+	for(int i=1; i < dval.length(); i++)
+		result = (result * 10) + key.indexOf(dval.charAt(i));
+	
+	return result;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(getValue("TRADINGFEW", "LGXWEV"));
