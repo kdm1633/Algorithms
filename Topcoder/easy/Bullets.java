8fe3751b36721805c64490bb2375064ed896370a
kdm1633 <kdm1633@outlook.com>
2016-03-26 09:57:25 +0900

    Update Bullets.java

--- Topcoder/easy/Bullets.java
+++ Topcoder/easy/Bullets.java
@@ -1,21 +1,21 @@
 public class Bullets {
-    public static int match(String[] guns, String bullet) {
-        int len = bullet.length();
+	public static int match(String[] guns, String bullet) {
+		int len = bullet.length();
         
-        for(int i=0; i < guns.length; i++) {
-            for(int j=0; j < len; j++) {
-                if(guns[i].equals(bullet))
-                    return i;
-                else
-                    guns[i] = guns[i].substring(1) + guns[i].substring(0,1);
-            }
-        }
-        
-        return -1;
-    }
-
+	        for(int i=0; i < guns.length; i++) {
+	            for(int j=0; j < len; j++) {
+	                if(guns[i].equals(bullet))
+	                    return i;
+	                else
+	                    guns[i] = guns[i].substring(1) + guns[i].substring(0,1);
+	            }
+	        }
+	        
+	        return -1;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(match(new String[]{"| | | |","|| || |"," |||| "}, "|| || |"));
+	    	System.out.println(match(new String[]{"| | | |","|| || |"," |||| "}, "|| || |"));
 		System.out.println(match(new String[]{"||| |","| | || "}, "|||| "));
 		System.out.println(match(new String[]{"|| || ||","| | | | ","||||||||"}, "||| ||| "));
 		System.out.println(match(new String[]{}, "| | | |"));
