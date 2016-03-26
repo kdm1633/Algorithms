9a045a4ade3a7932daa9b0d7a1199919c779629f
kdm1633 <kdm1633@outlook.com>
2016-03-26 09:58:53 +0900

    Update StreetParking.java

--- Topcoder/easy/StreetParking.java
+++ Topcoder/easy/StreetParking.java
@@ -7,18 +7,18 @@ public class StreetParking {
             if(street.charAt(i) == '-') {
                 if(i-1 >= 0 && street.charAt(i-1) == 'S') continue;
                 if(i+1 < len && (street.charAt(i+1) == 'B' || street.charAt(i+1) == 'S')) continue;
-				if(i+2 < len && street.charAt(i+2) == 'B') continue;
-				tot++;
+		if(i+2 < len && street.charAt(i+2) == 'B') continue;
+		tot++;
             }
         }
         
         return tot;
     }
-
-	public static void main(String[] args) {
-		System.out.println(freeParks("---B--S-D--S--"));
-		System.out.println(freeParks("DDBDDBDDBDD"));
-		System.out.println(freeParks("--S--S--S--S--"));
-		System.out.println(freeParks("SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D"));
-	}
+    
+    public static void main(String[] args) {
+	System.out.println(freeParks("---B--S-D--S--"));
+	System.out.println(freeParks("DDBDDBDDBDD"));
+	System.out.println(freeParks("--S--S--S--S--"));
+	System.out.println(freeParks("SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D"));
+    }
 }
