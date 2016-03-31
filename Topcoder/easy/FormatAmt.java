7c2caa8a0b56d403878146e26fbb8e7569cd74c6
kdm1633 <kdm1633@outlook.com>
2016-03-31 12:48:12 +0900

    Added files via upload

---
+++ Topcoder/easy/FormatAmt.java
@@ -0,0 +1,19 @@
+public class FormatAmt {
+	public static String amount(int dollars, int cents) {
+		String str = Integer.toString(dollars);
+		String res = ((cents < 10) ? ".0" : ".") + Integer.toString(cents);
+		
+		for(int i=str.length(); i > 0; i-=3)
+			res = (i > 3) ? "," + str.substring(i-3, i) + res : "$" + str.substring(0,i) + res;
+		
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(amount(123456,0));
+		System.out.println(amount(49734321,9));
+		System.out.println(amount(0,99));
+		System.out.println(amount(249,30));
+		System.out.println(amount(1000,1));
+	}
+}
