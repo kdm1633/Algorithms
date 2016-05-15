e9864d0c45c5f941ea068656f338d7f1fee1bd10
kdm1633 <kdm1633@outlook.com>
2016-05-15 14:51:14 +0900

    Update Checkers.java

--- Topcoder/easy/Checkers.java
+++ Topcoder/easy/Checkers.java
@@ -1,26 +1,26 @@
 public class Checkers
 {
-    int min;
-    char[][] board;
-
-    public int compute(String startPos, String[] pieces) {
-        min = 7;
+	int min;
+	char[][] board;
+	
+	public int compute(String startPos, String[] pieces) {
+		min = 7;
 		board = new char[8][8];
-
-        int x = startPos.charAt(0) - '0';
-        int y = startPos.charAt(2) - '0';
-        
-        for(int i=0; i < pieces.length; i++)
-            board[pieces[i].charAt(0) - '0'][pieces[i].charAt(2) - '0'] = 'B';
-
-        search(x,y,0,false);
-
-        if(min == 7) min = -1;
-
-        return min;
-    }
-
-    public void search(int x, int y, int count, boolean jump) {
+		
+		int x = startPos.charAt(0) - '0';
+		int y = startPos.charAt(2) - '0';
+		
+		for(int i=0; i < pieces.length; i++)
+			board[pieces[i].charAt(0) - '0'][pieces[i].charAt(2) - '0'] = 'B';
+		
+		search(x,y,0,false);
+		
+		if(min == 7) min = -1;
+		
+		return min;
+	}
+	
+	public void search(int x, int y, int count, boolean jump) {
 		if(y==7) {
 			if(count < min) min = count;
 			return;
@@ -51,17 +51,17 @@ public class Checkers
 					search(rr, uu, (jump) ? count : count+1, true);
 			}
 		}
-    }
-
-    public static void main(String[] args) {
-        Checkers c = new Checkers();
-        System.out.println(c.compute("1,0", new String[]{"2,1", "0,3", "4,3", "5,6", "4,2"}));
-        System.out.println(c.compute("4,4", new String[]{}));
-        System.out.println(c.compute("4,4", new String[]{"6,6", "5,5", "3,5", "2,6"}));
-        System.out.println(c.compute("4,1", new String[]{"2,4", "3,4", "4,4", "5,4", "2,6", "3,6", "4,6", "5,6"}));
-        System.out.println(c.compute("0,4", new String[]{"1,7","3,7"}));
-        System.out.println(c.compute("0,7", new String[]{}));
-        System.out.println(c.compute("5,0", new String[]{"4,1", "2,3", "1,3", "3,4", "6,3", "2,5", "4,5"}));
-        System.out.println(c.compute("1,1", new String[] {"2,2", "0,4", "1,4", "2,4", "5,5", "4,6", "7,7"}));
-    }
+	}
+	
+	public static void main(String[] args) {
+		Checkers c = new Checkers();
+		System.out.println(c.compute("1,0", new String[]{"2,1", "0,3", "4,3", "5,6", "4,2"}));
+		System.out.println(c.compute("4,4", new String[]{}));
+		System.out.println(c.compute("4,4", new String[]{"6,6", "5,5", "3,5", "2,6"}));
+		System.out.println(c.compute("4,1", new String[]{"2,4", "3,4", "4,4", "5,4", "2,6", "3,6", "4,6", "5,6"}));
+		System.out.println(c.compute("0,4", new String[]{"1,7","3,7"}));
+		System.out.println(c.compute("0,7", new String[]{}));
+		System.out.println(c.compute("5,0", new String[]{"4,1", "2,3", "1,3", "3,4", "6,3", "2,5", "4,5"}));
+		System.out.println(c.compute("1,1", new String[] {"2,2", "0,4", "1,4", "2,4", "5,5", "4,6", "7,7"}));
+	}
 }
