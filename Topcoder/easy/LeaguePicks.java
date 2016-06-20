0504c2b0f8562917bd9a44e7e07bbab51610bd79
kdm1633 <kdm1633@outlook.com>
2016-06-20 09:55:36 +0900

    Create LeaguePicks.java

---
+++ Topcoder/easy/LeaguePicks.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class LeaguePicks
+{
+	public int[] returnPicks(int pos, int fr, int picks) {
+		int cnt=0;
+
+		for(int i=pos, j=0, sw=1; i <= picks; i += (sw==1) ? (fr-pos)*2+1 : pos*2-1, sw*=-1, cnt++) {}
+
+		int[] res = new int[cnt];
+
+		for(int i=pos, j=0, sw=1; i <= picks; i += (sw==1) ? (fr-pos)*2+1 : pos*2-1, sw*=-1)
+			res[j++] = i;
+		
+		return res;
+	}
+
+	public static void main(String[] args) {
+		LeaguePicks l = new LeaguePicks();
+		System.out.println(Arrays.toString(l.returnPicks(3,6,15)));
+		System.out.println(Arrays.toString(l.returnPicks(1,1,10)));
+		System.out.println(Arrays.toString(l.returnPicks(1,2,39)));
+		System.out.println(Arrays.toString(l.returnPicks(5,11,100)));
+	}
+}
