4adefeb2b9dfc8813c5bc5368224eb82cc1b9325
kdm1633 <kdm1633@outlook.com>
2016-07-30 16:27:36 +0900

    Create BearBall.java

---
+++ Topcoder/easy/BearBall.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.awt.Point;
+
+public class BearBall
+{
+	public int gcd(int a, int b) {
+		return b==0 ? a : gcd(b,a%b);
+	}
+
+	public int countThrows(int[] x, int[] y) {
+		int n = x.length;
+		int res = n * (n-1) *2;
+
+		for (int i=0; i < n; i++) {
+			HashSet<Point> set = new HashSet<Point>();
+
+			for (int j=0; j < n; j++) {
+				if(i==j) continue;
+
+				int dx = x[j] - x[i], dy = y[j] - y[i];
+				int g = gcd(Math.abs(dx),Math.abs(dy));
+				dx /= g; dy /= g;
+				
+				set.add(new Point(dx,dy));
+			}
+
+			if(set.size()==1) return n*(n-1)*(n+1)/3;
+			res -= set.size();
+		}
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		BearBall b = new BearBall();
+		System.out.println(b.countThrows(new int[]{1,4,2}, new int[]{1,10,7}));
+		System.out.println(b.countThrows(new int[]{0,0,0,1,1}, new int[]{0,1,2,0,1}));
+		System.out.println(b.countThrows(new int[]{5,7,9,11}, new int[]{4,3,2,1}));
+		System.out.println(b.countThrows(new int[]{10,10,50,50,90,90}, new int[]{5,17,5,17,5,17}));
+		System.out.println(b.countThrows(new int[]{-100, -90, -80, -70, -85, -90, 0, 20}, new int[]{-5, -8, -13, -21, -13, -13, -13, -69}));
+	}
+}
