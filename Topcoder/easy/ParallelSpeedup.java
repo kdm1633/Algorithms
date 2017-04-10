7543be96f03619541664308f41f2eae0bb4fec8f
kdm1633 <kdm1633@outlook.com>
2017-04-10 17:30:02 +0900

    Create ParallelSpeedup.java

---
+++ Topcoder/easy/ParallelSpeedup.java
@@ -0,0 +1,23 @@
+public class ParallelSpeedup
+{
+	public int numProcessors(int k, int overhead) {
+		int num=1, min=k;
+		for (int i=2, j=0, d=1; ; i++, j += d++) {
+			int cycle = (k%i==0) ? k/i : k/i+1;
+			int time = cycle + (j+d)*overhead;
+			if(time >= min) break;
+			num = i; min = time;
+		}
+
+		return num;
+	}
+
+	public static void main(String[] args) {
+		ParallelSpeedup p = new ParallelSpeedup();
+		System.out.println(p.numProcessors(12,1));
+		System.out.println(p.numProcessors(50,3));
+		System.out.println(p.numProcessors(9,10));
+		System.out.println(p.numProcessors(3333,2));
+		System.out.println(p.numProcessors(1000000,4));
+	}
+}
