a0bd721f18538ca6f349d8f6da1041cc98dc1287
kdm1633 <kdm1633@outlook.com>
2017-04-10 19:00:23 +0900

    Create MissingLetters.java

---
+++ Topcoder/easy/MissingLetters.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class MissingLetters
+{
+	String getMissingLetters(String sentence) {
+		sentence = sentence.toLowerCase();
+
+		String res = "";
+		for (char c='a'; c <= 'z'; c++)
+			if (sentence.indexOf(c) == -1)
+				res += c;
+
+		char[] ch = res.toCharArray();
+		Arrays.sort(ch);
+		res = new String(ch).toUpperCase();
+
+		return res;
+	}
+
+	public static void main(String[] args) {
+		MissingLetters m = new MissingLetters();
+		System.out.println(m.getMissingLetters("A quick brown fox jumps over the lazy dog"));
+		System.out.println(m.getMissingLetters("A slow yellow fox crawls under the proactive dog"));
+		System.out.println(m.getMissingLetters("Lions, and tigers, and bears, oh my!"));
+		System.out.println(m.getMissingLetters(""));
+	}
+}
