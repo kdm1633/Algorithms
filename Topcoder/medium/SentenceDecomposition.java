ac81536d5fae186ad05c6338449aa08bfaf44c83
kdm1633 <kdm1633@outlook.com>
2018-05-16 03:26:16 +0900

    Update SentenceDecomposition.java

--- Topcoder/medium/SentenceDecomposition.java
+++ Topcoder/medium/SentenceDecomposition.java
@@ -35,5 +35,15 @@ public class SentenceDecomposition
 		
 		return (dp[n] != 51) ? dp[n] : -1;
 	}
-  
-  // https://community.topcoder.com/stat?c=problem_solution&cr=14927744&rd=12183&pm=8692
+	
+	public static void main(String[] args) {
+		SentenceDecomposition s = new SentenceDecomposition();
+		System.out.println(s.decompose("neotowheret",new String[]{"one", "two", "three", "there"}));
+		System.out.println(s.decompose("abba",new String[]{"ab", "ac", "ad"}));
+		System.out.println(s.decompose("thisismeaningless",new String[]{"this", "is", "meaningful"}));
+		System.out.println(s.decompose("ommwreehisymkiml",new String[]{"we", "were", "here", "my", "is", "mom", "here", "si", "milk", "where", "si"}));
+		System.out.println(s.decompose("ogodtsneeencs",new String[]{"go", "good", "do", "sentences", "tense", "scen"}));
+		System.out.println(s.decompose("sepawaterords",new String[]{"separate","words"}));
+	}
+}
+// https://community.topcoder.com/stat?c=problem_solution&cr=14927744&rd=12183&pm=8692
