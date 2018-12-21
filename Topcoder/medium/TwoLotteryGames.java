0b53bf4c0af6b2f0bcca77c9597261acb59a7233
kdm1633 <kdm1633@outlook.com>
2018-12-21 19:18:18 +0900

    Create TwoLotteryGames.java

---
+++ Topcoder/medium/TwoLotteryGames.java
@@ -0,0 +1,23 @@
+public class TwoLotteryGames {
+	long cbn(long n, long r) {
+		long denom=1, numer=1;
+		while(r > 0) {denom *= n--; numer *= r--;}
+		return denom / numer;
+	}
+
+	public double getHigherChanceGame(int n, int m, int k) {
+		long res=0;
+		for (int i=k; i <= m; i++)
+		  res += cbn(m,i) * cbn(n-m,m-i);
+
+		return (double)res/(double)cbn(n,m);
+	}
+
+	public static void main(String[] args) {
+		TwoLotteryGames t = new TwoLotteryGames();
+		System.out.println(t.getHigherChanceGame(3,2,1));
+		System.out.println(t.getHigherChanceGame(3,1,1));
+		System.out.println(t.getHigherChanceGame(8,2,1));
+		System.out.println(t.getHigherChanceGame(8,4,2));
+	}
+}
