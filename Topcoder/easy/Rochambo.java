08bc122cbe36d6e0e0aa9ffb79411eb0b603fe85
kdm1633 <kdm1633@outlook.com>
2019-05-27 05:05:40 +0900

    Update Rochambo.java

--- Topcoder/easy/Rochambo.java
+++ Topcoder/easy/Rochambo.java
@@ -7,8 +7,9 @@ public class Rochambo {
 		
 		for (int i=2; i < opponent.length(); i++) {
 			char c;
-			if(opponent.charAt(i-1) != opponent.charAt(i-2)) c = (char)('R' + 'P' + 'S' - opponent.charAt(i-1) - opponent.charAt(i-2));
+			if(opponent.charAt(i-2) != opponent.charAt(i-1)) c = (char)('R' + 'P' + 'S' - opponent.charAt(i-1) - opponent.charAt(i-2));
 			else c = opponent.charAt(i-1);
+			
 			if(c == opponent.charAt(i)) w++;
 		}
 		
@@ -26,33 +27,3 @@ public class Rochambo {
 }
 
 // https://community.topcoder.com/stat?c=problem_solution&cr=144400&rd=4620&pm=1810
-
-//public class Rochambo {
-//    public int wins(String opponent) {
-//        int w=0;
-//        if(opponent.charAt(0) == 'S') w++;
-//        if(opponent.charAt(1) == 'S') w++;
-//        
-//        String str;
-//        for (int i=2; i < opponent.length(); i++) {
-//            char prev1 = opponent.charAt(i-1), prev2 = opponent.charAt(i-2), p2 = opponent.charAt(i);
-//            
-//            char prediction;
-//            if(prev1 == prev2) prediction = prev1;
-//            else {
-//                if(prev1 == 'R') prediction = (prev2 == 'P') ? 'S' : 'P';
-//                else if(prev1 == 'P') prediction = (prev2 == 'R') ? 'S' : 'R';
-//                else prediction = (prev2 == 'R') ? 'P' : 'R';
-//            }
-//            
-//            char p1 = (prediction == 'R') ? 'P' : (prediction == 'P') ? 'S' : 'R';
-//            if(p2 != p1) {
-//                if(p2 == 'R' && p1 == 'P') w++;
-//                if(p2 == 'P' && p1 == 'S') w++;
-//                if(p2 == 'S' && p1 == 'R') w++;
-//            }
-//        }
-//        
-//        return w;
-//    }
-//}
