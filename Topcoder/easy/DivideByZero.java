e34d34f5b07b75a783839944973c8f086858dc23
kdm1633 <kdm1633@outlook.com>
2019-05-29 06:23:56 +0900

    Update DivideByZero.java

--- Topcoder/easy/DivideByZero.java
+++ Topcoder/easy/DivideByZero.java
@@ -1,38 +1,22 @@
-/* Code 1 */
-
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class DivideByZero
 {
 	public int CountNumbers(int[] numbers) {
-		Arrays.sort(numbers);
-		
-		boolean[] bmap = new boolean[101];
-		for (int i=0; i < numbers.length; i++)
-			bmap[numbers[i]] = true;
+		HashSet<Integer> paper = new HashSet<Integer>();
+		for (int i : numbers) paper.add(i);
 
-		int res = numbers.length;
-
-		for (int i=0; i < numbers.length; i++) {
-			if(numbers[i] == 1) continue;
-
-			for (int j=i+1; j < numbers.length; j++) {
-				int n = numbers[j]/numbers[i];
-				if(!bmap[n]) {
-					bmap[n] = true;
-					res++;
-					int[] temp = new int[numbers.length+1];
-					System.arraycopy(numbers, 0, temp, 0, numbers.length);
-					temp[temp.length-1] = n;
-					Arrays.sort(temp);
-					numbers = temp;
-					i=-1;
-					break;
-				}
-			}
+		int old = 0;
+		while (paper.size() > old) {
+			old = paper.size();
+			HashSet<Integer> clone = (HashSet)paper.clone();
+			for (int a : clone)
+				for (int b : clone)
+					if (a>b) paper.add(a/b);
 		}
 
-		return res;
+		return paper.size();
 	}
 
 	public static void main(String[] args) {
@@ -46,49 +30,4 @@ public class DivideByZero
 	}
 }
 
-/* Code 2 */
-
-//import java.util.Arrays;
-//import java.util.ArrayList;
-//import java.util.Collections;
-//
-//public class DivideByZero
-//{
-//	public int CountNumbers(int[] numbers) {
-//		System.out.println(System.currentTimeMillis());
-//		Arrays.sort(numbers);
-//
-//		ArrayList<Integer> nums = new ArrayList<Integer>();
-//
-//		for (int i=0; i < numbers.length; i++)
-//			nums.add(numbers[i]);
-//
-//		for (int i=0; i < nums.size(); i++) {
-//			if(nums.get(i) == 1) continue;
-//			for (int j=i+1; j < nums.size(); j++) {
-//				int n = nums.get(j)/nums.get(i);
-//				if(nums.indexOf(n) == -1) {
-//					nums.add(n);
-//					Collections.sort(nums);
-//					i=-1;
-//					break;
-//				}
-//			}
-//		}
-//
-//		if(numbers[0] == 1) return nums.size()+1;
-//		System.out.println(System.currentTimeMillis());
-//
-//		return nums.size();
-//	}
-//
-//	public static void main(String[] args) {
-//		DivideByZero d = new DivideByZero();
-//		System.out.println(d.CountNumbers(new int[]{9,2}));
-//		System.out.println(d.CountNumbers(new int[]{8,2}));
-//		System.out.println(d.CountNumbers(new int[]{50}));
-//		System.out.println(d.CountNumbers(new int[]{1,5,8,30,15,4}));
-//		System.out.println(d.CountNumbers(new int[]{1,2,4,8,16,32,64}));
-//		System.out.println(d.CountNumbers(new int[]{6,2,18}));
-//	}
-//}
+// https://apps.topcoder.com/wiki/display/tc/SRM+610
