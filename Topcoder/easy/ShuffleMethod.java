5d8be42bebd993fa5a2c5d9cad0bcd5ae17e5dd7
kdm1633 <kdm1633@outlook.com>
2019-10-04 18:13:55 +0900

    Update ShuffleMethod.java

--- Topcoder/easy/ShuffleMethod.java
+++ Topcoder/easy/ShuffleMethod.java
@@ -2,101 +2,81 @@ import java.util.Arrays;
 
 public class ShuffleMethod
 {
-	int n,f,cnt;
-	boolean[] fixed, used;
+	int n;
+
 	int[] ts;
-	int[] sn;					// sn: selected numbers
-	int[][] pn;					// pn: possible numbers
-	int[] idx;
-	
-	boolean search(int[] r) {
-		for (int a=r[0]; idx[a] < n-2-f; idx[a]++) {
-			sn[a] = pn[a][idx[a]];
-			used[sn[a]] = true;
-			
-			int i = a;
-			cnt = 1+ts.length-r.length;
-			while (true) {
-				if (sn[sn[i]] != 0) {
-					if (sn[sn[i]] == ts[i-1]) {
-						int cnt1=0;
-						for (int k=0; k < r.length; k++)
-							if(!used[r[k]]) cnt1++;
-						
-						int[] temp = new int[cnt1];
-						for (int k=0,l=0; k < r.length; k++)
-							if(!used[r[k]]) temp[l++] = r[k];
-						
-						return search(temp);
-					}
-					else break;
+
+	boolean[] flag;
+	int[] sn;				// selected number
+	int[][] pn;				// possible number
+
+	boolean visit(int cnt) {
+		for (int i = 0; i < n; i++) {
+			if (flag[i+1]) continue;
+
+			for (int j = 0; j < pn[i].length; j++) {
+				if (flag[pn[i][j]]) continue;
+
+				sn[i] = pn[i][j];
+				flag[sn[i]] = true;
+
+				int a = sn[i]-1;
+				int b = i;
+				int cnt1 = cnt+1;
+				boolean[] used = new boolean[n];
+				for (int k = 0; k < pn[a].length; k++) {
+					if (flag[pn[a][k]] || pn[a][k]!=ts[b] || sn[a]!=0) continue;
+
+					sn[a] = ts[b];
+					flag[sn[a]] = true;
+					used[a] = true;
+					cnt1++;
+					if (cnt1 == n) return true;
+					b = a;
+					a = sn[a]-1;
+					k = -1;
+					if (ts[b] == sn[a]) return visit(cnt1);
 				}
-				
-				for (int j=sn[i]; idx[j] < n-2-f; idx[j]++) {
-					if (pn[j][idx[j]] == ts[i-1] && !used[ts[i-1]]) {
-						sn[j] = ts[i-1]; break;
-					}
-				}
-				
-				if(sn[sn[i]] == 0) break;
-				
-				if(++cnt == ts.length) return true;
-				
-				i = sn[i]; used[sn[i]] = true;
+
+				flag[sn[i]] = false;
+
+				for (int k = 0; k < n; k++)
+					if (used[k]) {flag[sn[k]] = false; sn[k] = 0;}
 			}
-			
-			for (int j=0; j < r.length; j++) {
-				used[r[j]] = false;
-				sn[r[j]] = 0;
-			}
-			for (int j=1; j < r.length; j++)
-				idx[r[j]] = 1;
 		}
-		
+
 		return false;
 	}
 
 	public int[] oneTime(int[] twoShuffle) {
+		n = twoShuffle.length;
 		ts = twoShuffle;
-		n = twoShuffle.length+1;
-		f = 0;
 
-		fixed = new boolean[n];
-		used = new boolean[n];
-		idx = new int[n];
+		flag = new boolean[n+1];
 		sn = new int[n];
 
-		for (int i=1; i < n; i++) {
-			if (i == ts[i-1]) {
-				fixed[i] = true;
-				sn[i] = i;
-				f++;
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (i+1 == ts[i]) {
+				flag[i+1] = true;
+				sn[i] = i+1;
+				cnt++;
 			}
 		}
-		
-		if(f == ts.length) return ts;
+		if (cnt == n) return sn;
 
-		pn = new int[n][n-2-f];
-		for (int i=1; i < n; i++) {
-			if(fixed[i]) continue;
-			for (int j=1, k=1; j < n; j++) {
-				if(i==j || j==twoShuffle[i-1] || fixed[j]) continue;
-				pn[i][k++] = j;
+		pn = new int[n][n-2-cnt];
+
+		for (int i = 0; i < n; i++) {
+			if (flag[i+1]) continue;
+
+			for (int j = 0, k = 0; j < n; j++) {
+				if (i==j || j+1==ts[i] || flag[j+1]) continue;
+				pn[i][k++] = j+1;
 			}
 		}
-		
-		for (int i=1; i < n; i++)
-			idx[i] = 1;
-		
-		int[] r = new int[ts.length-f];
-		for (int i=0,j=1; j < n; j++)
-			if(!fixed[j]) r[i++] = j;
-		
-		if(search(r)) {
-			int[] res = new int[n-1];
-			System.arraycopy(sn,1,res,0,res.length);
-			return res;
-		}
+
+		if (visit(cnt)) return sn;
 
 		return new int[0];
 	}
@@ -109,5 +89,6 @@ public class ShuffleMethod
 		System.out.println(Arrays.toString(s.oneTime(new int[]{2,4,6,5,1,8,10,9,3,12,11,13,7,15,16,17,14})));
 		System.out.println(Arrays.toString(s.oneTime(new int[]{2,4,6,5,1,8,10,9,3,12,11,13,7})));
 		System.out.println(Arrays.toString(s.oneTime(new int[]{10,9,12,8,13,3,4,1,5,11,6,2,7})));
+		System.out.println(Arrays.toString(s.oneTime(new int[]{7, 46, 13, 47, 48, 33, 23, 45, 10, 37, 12, 26, 16, 42, 34, 15, 50, 11, 44, 39, 17, 25, 38, 28, 27, 21, 5, 40, 36, 35, 31, 14, 1, 19, 24, 30, 43, 29, 9, 6, 49, 4, 8, 20, 3, 41, 22, 2, 32, 18})));
 	}
 }
