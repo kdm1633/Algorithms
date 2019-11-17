281ad829d6a0e10e5922905242f17efa8c580b60
kdm1633 <kdm1633@outlook.com>
2019-11-17 20:08:33 +0900

    Update SetComparison.java

--- Topcoder/easy/SetComparison.java
+++ Topcoder/easy/SetComparison.java
@@ -1,166 +1,48 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.TreeSet;
+import java.util.StringTokenizer;
 
 public class SetComparison
 {
-	String[] r = {"EQUIPOTENT","EQUIVALENT","MEMBER","POWERSET","PROPER SUBSET","SUBSET"};
-	
-	boolean[] chk;
-	
-	TreeSet<String> sa,sb;
-	
-	long comb(long n, long r) {
-		long denom=1, numer=1;
-		
-		while (r > 0) {denom *= n--; numer *= r--;}
-		
-		return denom / numer;
-	}
-
-	boolean equipotent() {
-		if(sa.size() == sb.size()) return true;
-		
-		return false;
-	}
-	
-	boolean equivalent() {
-		if(sa.size() != sb.size()) return false;
-		
-		for (String s : sa)
-			if(!sb.contains(s)) return false;
-		
-		return true;
-	}
-	
-	boolean member() {
-		if (sa.size()==0) {
-			if(sb.contains("{}")) return true;
-			return false;
+	HashSet getSet(StringTokenizer st) {
+		HashSet s = new HashSet<String>();
+		String t = st.nextToken();
+		while (!t.equals("}")) {
+			if (t.equals("{")) s.add(getSet(st));
+			else if (!t.equals(",")) s.add(t);
+			t = st.nextToken();
 		}
 
-		String a = "";
-		for (String s : sa)
-			a += s+",";
-
-		System.out.println(a);
-		a = "{" + a.substring(0,a.length()-1) + "}";
-		
-		if(sb.contains(a)) return true;
-		
-		return false;
+		return s;
 	}
 	
-	boolean powerset() {
-		int pSize = 1;
-		for (int i=1; i <= sb.size(); i++)
-			pSize += comb(sb.size(),i);
-		
-		if(sa.size() != pSize) return false;
-		
-		for (String s : sa) {
-			if(s.charAt(0) != '{') return false;
+	public String[] relation(String _a, String _b) {
+		StringTokenizer st = new StringTokenizer(_a,"{},",true);
+		st.nextToken();
+		HashSet a = getSet(st);
+		st = new StringTokenizer(_b,"{},",true);
+		st.nextToken();
+		HashSet b = getSet(st);
+		TreeSet<String> ret = new TreeSet<String>();
 
-			TreeSet<String> hs = new TreeSet<String>();
-			
-			for (int i=1; i < s.length()-1; ) {
-				if (s.charAt(i) != '{') {
-					int idx = s.indexOf(',',i+1);
-					if(idx == -1) idx = s.length()-1;
-					if(!sb.contains(s.substring(i,idx))) return false;
-					i = idx+1;
-				}
-				else if (s.charAt(i) == '{') {
-					int cnt=1;
-					int j = i+1;
-					for (; j < s.length(); j++) {
-						if(s.charAt(j) == '{') cnt++;
-						else if(s.charAt(j) == '}' && --cnt==0) break;
-					}
-					if(!sb.contains(s.substring(i+1,j))) return false;
-					i = j+2;
-				}
-			}
-			
-			for (String s1 : hs)
-				if(!sb.contains(s1)) return false;
+		if (b.contains(a)) ret.add("MEMBER");
+		boolean eq = a.equals(b);
+		if (eq) ret.add("EQUIVALENT");
+		if (b.containsAll(a)) {
+			ret.add("SUBSET");
+			if (!eq) ret.add("PROPER SUBSET");
 		}
-		
-		return true;
-	}
-	
-	boolean propersubset() {
-		if(chk[1] || chk[3]) return false;
-		
-		for (String s : sa)
-			if(!sb.contains(s)) return false;
-		
-		return true;
-	}
-	
-	boolean subset() {
-		if(chk[3]) return false;
+		if (a.size() == 1<<b.size()) {
+			boolean isPowerset = true;
+			for (Object o : a)
+				if (b.contains(o)) {isPowerset = false; break;}
 
-		for (String s : sa)
-			if(!sb.contains(s)) return false;
-
-		return true;
-	}
-	
-	TreeSet<String> saveElements(String str) {
-		TreeSet<String> hs = new TreeSet<String>();
-
-		for (int i=0; i < str.length(); ) {
-			if (str.charAt(i) == '{') {
-				if(str.charAt(i+1) == '}') {hs.add("{}"); i += 3; continue;}
-				
-				int cnt=1;
-				int j = i+1;
-				for (; j < str.length(); j++) {
-					if(str.charAt(j) == '{') cnt++;
-					else if(str.charAt(j) == '}' && --cnt==0) break;
-				}
-				
-				TreeSet<String> temp = saveElements(str.substring(i+1,j));
-				
-				String subset = "";
-				for (String s : temp)
-					subset += s+",";
-				subset = "{" + subset.substring(0,subset.length()-1) + "}";
-				hs.add(subset);
-				i = j+2;
-			}
-			else {
-				int idx = str.indexOf(',',i+1);
-
-				if(idx == -1) idx = str.length();
-
-				hs.add(str.substring(i,idx));
-				i = idx+1;
-			}
+			if (isPowerset) ret.add("POWERSET");
 		}
-		
-		return hs;
-	}
-	
-	public String[] relation(String a, String b) {
-		chk = new boolean[6];
+		if (a.size() == b.size()) ret.add("EQUIPOTENT");
 
-		sa = saveElements(a.substring(1,a.length()-1));
-		sb = saveElements(b.substring(1,b.length()-1));
-		
-		int len=0;
-		if(equipotent()) {chk[0] = true; len++;}
-		if(equivalent()) {chk[1] = true; len++;}
-		if(member()) {chk[2] = true; len++;}
-		if(powerset()) {chk[3] = true; len++;}
-		if(propersubset()) {chk[4] = true; len++;}
-		if(subset()) {chk[5] = true; len++;}
-		
-		String[] res = new String[len];
-		for (int i=0,j=0; i < chk.length; i++)
-			if(chk[i]) res[j++] = r[i];
-		
-		return res;
+		return ret.toArray(new String[0]);
 	}
 
 	public static void main(String[] args) {
@@ -170,8 +52,10 @@ public class SetComparison
 		System.out.println(Arrays.toString(s.relation("{a,b,c,d,e}","{a,a,b,b,c,c,d,d,e,e}")));	// equipotent, equivalent, subset
 		System.out.println(Arrays.toString(s.relation("{{a}}","{{{a}},{a}}"))); // member, proper subset, subset
 		System.out.println(Arrays.toString(s.relation("{a,a,{a},{a},{a,a,a},{},{},a,aa}","{a,{a},{},aa}"))); // equipotent, equivalent, subset
-		System.out.println(Arrays.toString(s.relation("{frank,bob}","{{},{frank,frank},{bob},{frank,bob,bob},{}}"))); //member
+		System.out.println(Arrays.toString(s.relation("{frank,bob}","{{},{frank,frank},{bob},{frank,bob,bob},{}}"))); // member
 		System.out.println(Arrays.toString(s.relation("{{a,b},{a},{a,b},{},{b},{b,a}}","{a,b}"))); // powerset
 		System.out.println(Arrays.toString(s.relation("{a,b,c,d}","{a,b}"))); // {}
 	}
 }
+
+// https://community.topcoder.com/stat?c=problem_solution&cr=287266&rd=4664&pm=1636
