3827c36592220996a0b43022e30afd3d3a0b82dc
kdm1633 <kdm1633@outlook.com>
2019-11-21 07:46:38 +0900

    Update SkipRope.java

--- Topcoder/easy/SkipRope.java
+++ Topcoder/easy/SkipRope.java
@@ -3,25 +3,19 @@ import java.util.Arrays;
 public class SkipRope
 {
 	public int[] partners(int[] candidates, int height) {
+		Arrays.sort(candidates);
+
 		int[] res = new int[2];
-		int[] weight = {101,101};
-		
-		int idx = 0;
-		for (int i=0; i < candidates.length; i++) {
-			int curWeight = Math.abs(candidates[i]-height);
-			if(curWeight==weight[0] && candidates[i]>res[0]) {res[0] = candidates[i]; idx = i;}
-			else if(curWeight < weight[0]) {res[0] = candidates[i]; weight[0] = curWeight; idx = i;}
+
+		int best = 201;
+		for (int i = 0; i < candidates.length-1; i++) {
+			if (Math.abs(candidates[i] - height) + Math.abs(candidates[i+1] - height) <= best) {
+				best = Math.abs(candidates[i] - height) + Math.abs(candidates[i+1] - height);
+				res[0] = candidates[i]; res[1] = candidates[i+1];
+			}
+			else break;
 		}
-		
-		for (int i=0; i < candidates.length; i++) {
-			if(idx == i) continue;
-			int curWeight = Math.abs(candidates[i]-height);
-			if(curWeight==weight[1] && candidates[i]>res[1]) {res[1] = candidates[i];}
-			else if(curWeight < weight[1]) {res[1] = candidates[i]; weight[1] = curWeight;}
-		}
-		
-		Arrays.sort(res);
-		
+
 		return res;
 	}
 
@@ -31,7 +25,6 @@ public class SkipRope
 		System.out.println(Arrays.toString(s.partners(new int[]{102, 97, 104},100)));
 		System.out.println(Arrays.toString(s.partners(new int[]{101, 100, 99},100)));
 		System.out.println(Arrays.toString(s.partners(new int[]{75, 117, 170, 175, 168, 167, 167, 142, 170, 85, 89, 170},169)));
-		System.out.println(Arrays.toString(s.partners(new int[]{134, 79, 164, 86, 131, 78, 99, 150, 105, 163, 150, 110, 90, 137, 127, 130, 121, 
- 93, 97, 131, 170, 137, 171, 153, 137, 138, 92, 103, 149, 110, 156},82)));
+		System.out.println(Arrays.toString(s.partners(new int[]{134, 79, 164, 86, 131, 78, 99, 150, 105, 163, 150, 110, 90, 137, 127, 130, 121, 93, 97, 131, 170, 137, 171, 153, 137, 138, 92, 103, 149, 110, 156},82)));
 	}
 }
