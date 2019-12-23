870ab15854a46d108448ce1626786f870ba6c9f2
kdm1633 <kdm1633@outlook.com>
2019-12-23 20:25:08 +0900

    Update ArithmeticSequenceDiv1.java

--- Topcoder/easy/ArithmeticSequenceDiv1.java
+++ Topcoder/easy/ArithmeticSequenceDiv1.java
@@ -1,22 +1,22 @@
+import java.util.Arrays;
+
 public class ArithmeticSequenceDiv1 {
 	public int findMinCost(int[] x) {
 		int n = x.length;
 		int min = 10000;
-		int maxGap = 0;
 
-		for (int i = 0; i < n-1; i++)
-			if(Math.abs(x[i+1]-x[i]) > maxGap) maxGap = Math.abs(x[i+1]-x[i]);
+		int[] y = new int[n];
+		for (int d = -100; d <= 100; d++) {
+			for (int i = 0; i < n; i++)
+				y[i] = x[i] - i*d;
 
-		for (int d = -maxGap; d <= maxGap; d++) {
-			for (int a = -200; a <= 200; a++) {
-				int sum = 0;
-				int yi = a;
-				for (int i = 0; i < n; i++) {
-					sum += Math.abs(yi-x[i]);
-					yi += d;
-				}
-				if(sum < min) min = sum;
-			}
+			Arrays.sort(y);
+			int median = y[n/2];
+			int sum = 0;
+			for (int i = 0; i < n; i++)
+				sum += Math.abs(y[i] - median);
+
+			if (sum < min) min = sum;
 		}
 
 		return min;
@@ -33,3 +33,32 @@ public class ArithmeticSequenceDiv1 {
 		System.out.println(a.findMinCost(new int[]{100,0,100,0,100}));
 	}
 }
+
+// https://www.topcoder.com/2018-tco-algorithm-round-2a-editorials/
+
+// My solution
+
+//public class ArithmeticSequenceDiv1 {
+//	public int findMinCost(int[] x) {
+//		int n = x.length;
+//		int min = 10000;
+//		int maxGap = 0;
+//
+//		for (int i = 0; i < n-1; i++)
+//			if(Math.abs(x[i+1]-x[i]) > maxGap) maxGap = Math.abs(x[i+1]-x[i]);
+//
+//		for (int d = -maxGap; d <= maxGap; d++) {
+//			for (int a = -200; a <= 200; a++) {
+//				int sum = 0;
+//				int yi = a;
+//				for (int i = 0; i < n; i++) {
+//					sum += Math.abs(yi-x[i]);
+//					yi += d;
+//				}
+//				if(sum < min) min = sum;
+//			}
+//		}
+//
+//		return min;
+//	}
+//}
