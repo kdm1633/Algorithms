b3b28d5065d32c5a7a13c942ab8e4798fedff0ef
kdm1633 <kdm1633@outlook.com>
2020-03-01 15:15:21 +0900

    Update ImageDithering.java

--- Topcoder/easy/ImageDithering.java
+++ Topcoder/easy/ImageDithering.java
@@ -1,34 +1,27 @@
-import java.util.Arrays;
-
 public class ImageDithering
 {
-	public int count(String dithered, String[] screen) {
-		int cnt=0;
-		char[] d = dithered.toCharArray();
+	public int count (String dithered, String[] screen) {
+		int num = 0;
+		for (int i = 0; i < screen.length; i++)
+			for (int j = 0; j < screen[0].length(); j++)
+				if (dithered.indexOf(screen[i].charAt(j)) != -1)
+					num++;
 
-		Arrays.sort(d);
-
-		for(int i=0; i < screen.length; i++) {
-			char[] s = screen[i].toCharArray();
-			
-			Arrays.sort(s);
-
-			for(int j=0,k=0; j < s.length && k < d.length; ) {
-				if(d[k] == s[j]) {cnt++; j++;}
-				else if(d[k] < s[j]) k++;
-				else j++;
-			}
-		}
-
-		return cnt;
+		return num;
 	}
 
 	public static void main(String[] args) {
-		ImageDithering d = new ImageDithering();
-		System.out.println(d.count("BW",new String[]{"AAAAAAAA","ABWBWBWA","AWBWBWBA","ABWBWBWA","AWBWBWBA","AAAAAAAA"}));
-		System.out.println(d.count("BW",new String[]{"BBBBBBBB", "BBWBWBWB", "BWBWBWBB", "BBWBWBWB", "BWBWBWBB", "BBBBBBBB"}));
-		System.out.println(d.count("ACEGIKMOQSUWY",new String[]{"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX","ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX","ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX","ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX","ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX","ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX"}));
-		System.out.println(d.count("CA",new String[]{"BBBBBBB","BBBBBBB","BBBBBBB"}));
-		System.out.println(d.count("DCBA",new String[]{"ACBD"}));
+		DitherCounter d = new DitherCounter();
+		System.out.println(d.count("BW", new String[]{"AAAAAAAA","ABWBWBWA","AWBWBWBA","ABWBWBWA","AWBWBWBA","AAAAAAAA"}));
+		System.out.println(d.count("BW", new String[]{"BBBBBBBB","BBWBWBWB","BWBWBWBB","BBWBWBWB","BWBWBWBB","BBBBBBBB"}));
+		System.out.println(d.count("ACEGIKMOQSUWY", new String[]{
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX",
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWX"}));
+		System.out.println(d.count("CA", new String[]{"BBBBBBB","BBBBBBB","BBBBBBB"}));
+		System.out.println(d.count("DCBA", new String[]{"ACBD"}));
 	}
 }
