f04228fd835e3bbebe0e93cc5c0135cec52629a4
kdm1633 <kdm1633@outlook.com>
2020-03-14 18:29:28 +0900

    Update RectangularGrid.java

--- Topcoder/easy/RectangularGrid.java
+++ Topcoder/easy/RectangularGrid.java
@@ -1,14 +1,14 @@
 public class RectangularGrid
 {
 	public long countRectangles(int width, int height) {
-		long res=0;
+		long res = 0;
 
-		for(int i=0; i < height; i++) {
-			for(int j=0; j < width; j++) {
-				if(i==j) continue;
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				if(i == j) continue;
 
 				res += (height-i)*(width-j);
-			}	
+			}
 		}
 
 		return res;
